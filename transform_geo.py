82b79041f3680dad6ea95db079924dc0c9e98e49
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2023-07-06 15:42:29 +0200

    geo transform

--- transform_geo.py
+++ transform_geo.py
@@ -1,15 +1,21 @@
+"""
+Das Skript, um die bezirksgrenzen.geojson-Datei 
+in die Form zu bringen, in der sie gerade ist.
+"""
+
 import json
 
 file = "static/bezirksgrenzen.geojson"
 
-def transform(feature):
-    feature["properties"]["Gemeinde_schluessel"] = int(feature["properties"]["Gemeinde_schluessel"])
-    return feature
-
-with open(file, "r") as f:
+with open(file, "r", encoding="utf-8") as f:
     data = json.load(f)
     
-data["features"] = [transform(feature) for feature in data["features"]]
+for feature in data["features"]:
+    props = feature["properties"]
+    feature["id"] = int(props["Gemeinde_schluessel"])
+    feature["Bezirk"] = props["Gemeinde_name"]
+    del feature["properties"]
+    
 
-with open(file, "w") as f:
+with open(file, "w", encoding="utf-8") as f:
     json.dump(data, f)
