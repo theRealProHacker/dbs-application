dd6fc72921739909b54876b90854fb246bd9e34d
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2023-07-15 14:24:23 +0200

    license and clean up

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-diebstähle = pd.read_csv("data/Fahrraddiebstahl.csv", encoding="ansi")
-plr = pd.read_csv("data/lor_planungsraeume_2021.csv", encoding="ansi")
-bezirke = pd.read_csv("data/bezirksgrenzen.csv", encoding="ansi")
-bezirke.rename(columns={"Gemeinde_schluessel": "BEZ"}, inplace=True)
-
-print(plr.join(bezirke, on="BEZ", lsuffix="_plr", rsuffix="_bezirke"))
