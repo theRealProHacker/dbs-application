a75bc85d7345050471b397fdc2635782ebf0d52b
Phili <schmip99@mi.fu-berlin.de>
2023-07-18 12:44:39 +0200

    zusammenfassen der tranform Dateien

--- transform_geo.py
+++ transform_geo.py
@@ -6,7 +6,9 @@ in die Form zu bringen, in der sie gerade ist.
 import json
 
 file = "static/bezirksgrenzen.geojson"
+filelor = "static/lor_planungsraeume_2021.geojson"
 
+#Bringt geojson-Datei mit Bezirken in Form
 
 with open(file, "r", encoding="utf-8") as f:
     data = json.load(f)
@@ -21,3 +23,17 @@ for feature in data["features"]:
 
 with open(file, "w", encoding="utf-8") as f:
     json.dump(data, f)
+
+#Bringt geojson-Datei mit Planungsraeumen in Form
+
+with open(filelor, "r", encoding="utf-8") as f:
+    data2 = json.load(f)
+
+for feature in data2["features"]: 
+    props = feature["properties"]
+    feature["PLR_ID"] = int(props["lor"])
+    feature["Planungsraum"] = props["lor_name"]
+    del feature["properties"]
+
+with open(filelor, "w", encoding="utf-8") as f:
+    json.dump(data2, f)
